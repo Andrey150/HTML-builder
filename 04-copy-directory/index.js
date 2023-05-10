266e46f6ab9a5c697e5080cc026cbbb0a5964ae6
Andrey150 <ananevandrey150@gmail.com>
2023-05-10 16:04:29 +0300

    fix: fix 4 task code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,23 @@
-let fs = require('fs');
+let fs = require('fs').promises;
 const path = require('path');
 const directory  = path.join(__dirname, 'files');
 const directoryNew  = path.join(__dirname, 'files-copy');
 
-fs.mkdir(directoryNew, { recursive: true }, err => {
-  if(err) throw err; // не удалось создать папку
-  console.log('Папка успешно создана');
-});
+async function copyFiles() {
+  try {
+    await fs.rm(directoryNew, { recursive: true });
 
+    await fs.mkdir(directoryNew, { recursive: true });
+    console.log('Папка успешно создана');
 
-fs.readdir(directoryNew, 'utf8', function(error, dirContent){
-  if(error) throw error; // ошибка чтения файла, если есть
-  dirContent.forEach((file) => {
-    fs.rm(`${directoryNew}/${file}`, err => {
-      if(err) throw err; // не удалось удалить файл
-      console.log('Файл успешно удалён');
-    });
-  });
-});
+    const dirContent = await fs.readdir(directory, 'utf8');
+    for (const file of dirContent) {
+      await fs.copyFile(path.join(directory, file), path.join(directoryNew, file));
+      console.log(`Файл успешно скопирован`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-fs.readdir(directory, 'utf8', function(error, dirContent){
-  if(error) throw error; // ошибка чтения файла, если есть
-  dirContent.forEach((file) => {
-    fs.copyFile(`${directory}/${file}`, `${directoryNew}/${file}`,  err => {
-      if(err) throw err; // не удалось скопировать файл
-      console.log('Файл успешно скопирован');
-    });
-  });
-});
+copyFiles();
